e25640c226680526715ca8c9e2919cc8536c0e57
Rishabh Jain <jrishabh55@outlook.com>
2016-04-14 21:45:03 +0530

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
-# catc-api
-This API Wrapper is created for the Cloud At Cost API users to easyout there flow...
+# Information
+```
+Name: CatC Wrapper
+Author: Rishabh Jain
+Website: https://www.hsrtech.net
+Stable Version: 1.0
+Minimum Requirement: PHP 5.6 with cURL extenssion
+```
+# Short Description
+PHP wrapper for the cloudatcost's API by HSR TECH
+
+# References
+[Could at Cost API](https://github.com/cloudatcost/api)
+
+[VPS-HOSTING.CA API](https://panel.vps-hosting.ca/api-details.php)
+
+# Usage
+
+### Basic Usage
+The following example will show the basic
+```php
+$api = new \hsrtech\catc\Wrapper(
+    [
+        'email' => 'Registered Email',
+        'api_key' => 'YourLoginEmail'
+    ]
+);
+```
+### Advance Usage
+There is the ability to use this on the other website having same kind of panel with adding some other parameters.
+```php
+$api = new \hsrtech\catc\Wrapper(
+    [
+        'email' => 'Registered Email',
+        'api_key' => 'YourLoginEmail'
+    ],
+    [
+        'link' => 'The Base link for the api', // Default:  https://panel.cloudatcost.com/api
+        'api_version' => 'the version of API to use', // Default = 'v1'
+    ]
+);
+
+```
+# Actions
+### List servers
+```php
+$api->getServers();
+```
+
+### List templates
+```php
+$api->getTemplates();
+```
+
+### List tasks
+```php
+$api->getTasks();
+```
+
+### Power ON server
+```php
+$api->powerOnServer($Server_ID);
+```
+
+### Power OFF server
+```php
+$api->powerOffServer($Server_ID);
+```
+
+### Reboot server
+```php
+$api->rebootServer($Server_ID);
+```
+
+### Get Console url
+```php
+$api->getConsole($Server_ID);
+```
+
+### Rename server
+```php
+$api->renameServer($Server_ID, $Name);
+```
+
+### Modify reverse DNS
+```php
+$api->setRDNS($Server_ID, $rdns);
+```
+
+### Run Mode
+```php
+$api->runMode($Server_ID, $runmode);
+```
+##
+## Cloud Pro Features
+
+### Build Server
+```php
+$api->buildServer(
+    [
+        'cpu' => 'Amount of CPU cores for server',
+        'ram' => 'Amount of Ram for the server',
+        'storage' => 'Storage space alloted to the server',
+        'os' => 'Valid Operating System ID',
+    ]
+);
+```
+### Delete Server
+```php
+$api->deleteServer($Server_ID);
+```
+
+### Resources
+```php
+$api->getResources();
+```
+
+##Extra Features
+You can also return the result of last call using following function.
+```php
+$api->results();
+```
+## Credits
+- [Rishabh Jain](https://www.fb.com/jrishbah55)
+- [HSR Tech](https://github.com/hsrtech)
+
+## License
+
+  Copyright 2016 [HSR-TECH](https://www.hsrtech.net)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
---
+++ src/Wrapper.php
@@ -0,0 +1,290 @@
+<?php   namespace hsrtech\catc;
+
+use \Curl\Curl;
+
+class Wrapper extends Curl
+{
+    /**
+     * @var mixed|string
+     */
+    private $_params,
+            $_base_link,
+            $_result;
+
+    /**
+     * @var mixed|string
+     */
+    private $_api_version = 'v1';
+
+    /**
+     * @array List of the commands
+     */
+    const ACTIONS = [
+            'servers'   =>  'listservers.php',
+            'templates' =>  'listtemplates.php',
+            'tasks'     =>  'listtasks.php',
+            'power'     =>  'powerop.php',
+            'rename'    =>  'renameserver.php',
+            'rdns'      =>  'rdns.php',
+            'console'   =>  'console.php',
+            'runmode'   =>  'runmode.php',
+            'build'     =>  'cloudpro/build.php',
+            'delete'    =>  'cloudpro/delete.php',
+            'resources' =>  'cloudpro/resources.php',
+        ];
+
+    /**
+     * Wrapper constructor.
+     * @param array $api_params
+     * @param array $extra_params
+     * @throws \ErrorException
+     * @throws \Exception
+     * @internal param array $params
+     */
+
+    public function __construct(array $api_params, array $extra_params)
+    {
+        parent::__construct();
+        if(empty($api_params['email']) || empty($api_params['api_key']))
+        {
+            return('User Email and API key is Required');
+        }
+
+        $this->_params = $api_params;
+        $this->_base_link = (!isset($extra_params['link']) || is_null($extra_params['link'])) ? "https://panel.cloudatcost.com/api": $extra_params['link'];
+        $this->_api_version = (!isset($extra_params['api_version']) || is_null($extra_params['api_version'])) ? "v1": $extra_params['api_version'];
+        return true;
+    }
+
+    /**
+     * Reterive the list of the Servers
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function getServers()
+    {
+        $this->request(self::ACTIONS['servers'],'GET');
+        return $this->result();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getTemplates(){
+        $this->request(self::ACTIONS['templates'],'GET');
+        return $this->result();
+    }
+
+    /**
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function getTasks()
+    {
+        $this->request(self::ACTIONS['tasks'],'GET');
+        return $this->result();
+    }
+
+    /**
+     * @param $Server_ID
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function powerOnServer($Server_ID)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        return $this->powerop($Server_ID,'poweron');
+    }
+
+
+    /**
+     * @param $Server_ID
+     * @return mixed
+     * @throws \Exception
+     */
+    public function powerOffServer($Server_ID)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        return $this->powerop($Server_ID,'poweroff');
+    }
+
+    /**
+     * @param $Server_ID
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function rebootServer($Server_ID)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        return $this->powerop($Server_ID,'reset');
+    }
+
+    /**
+     * @param $Server_ID
+     * @param $action
+     * @return mixed|string
+     * @throws \Exception
+     */
+    protected function powerop($Server_ID,$action)
+    {
+        $this->_params['sid'] = $Server_ID;
+        $this->_params['action'] = $action;
+        $this->request(self::ACTIONS['power']);
+        return (!empty($this->result()->result)) ? $this->result()->result : $this->result();
+    }
+
+    /**
+     * @param $Server_ID
+     * @param $action
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function runMode($Server_ID,$action)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+
+        $options = ['normal','safe'];
+        if(!in_array($action,$options)) return("Invalid Run Mode");
+
+        $this->_params['sid'] = $Server_ID;
+        $this->_params['mode'] = $action;
+        $this->request(self::ACTIONS['runmode']);
+        return (!empty($this->result()->result)) ? $this->result()->result : $this->result();
+    }
+
+    /**
+     * @param $Server_ID
+     * @param $name
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function renameServer($Server_ID,$name)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        $this->_params['sid'] = $Server_ID;
+        $this->_params['name'] = $name;
+        $this->request(self::ACTIONS['rename']);
+        return (!empty($this->result()->result)) ? $this->result()->result : $this->result();
+    }
+
+    /**
+     * @param $Server_ID
+     * @param $RDNS
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function setRDNS($Server_ID,$RDNS)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        $this->_params['sid'] = $Server_ID;
+        $this->_params['hostname'] = $RDNS;
+        $this->request(self::ACTIONS['rdns']);
+        return (!empty($this->result()->result)) ? $this->result()->result : $this->result();
+    }
+
+    /**
+     * @param $Server_ID
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function getConsole($Server_ID)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        $this->_params['sid'] = $Server_ID;
+        $this->request(self::ACTIONS['console']);
+        return (!empty($this->result()->console)) ? $this->result()->console : $this->result();
+    }
+
+    /**
+     * @param $params
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function buildServer($params)
+    {
+        if(empty($params['cpu']) || empty($params['ram']) || empty($params['storage']) || empty($params['os'])) return ("Incomplete Data Provided.");
+        if(!is_numeric($params['cpu']) || !is_numeric($params['ram']) || !is_numeric($params['storage']) || !is_numeric($params['os'])) return ("Invalid Data Provided.");
+        $this->_params['cpu'] = $params['cpu'];
+        $this->_params['ram'] = $params['ram'];
+        $this->_params['storage'] = $params['storage'];
+        $this->_params['os'] = $params['os'];
+        $this->request(self::ACTIONS['build']);
+        return (!empty($this->result()->result)) ? $this->result()->result : $this->result();
+    }
+
+    /**
+     * @param $Server_ID
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function deleteServer($Server_ID)
+    {
+        if(!is_numeric($Server_ID)) return('Invalid Server ID');
+        $this->_params['sid'] = $Server_ID;
+        $this->request(self::ACTIONS['delete']);
+        return (!empty($this->result()->result)) ? $this->result()->result : $this->result();
+    }
+
+    /**
+     * @return mixed|string
+     * @throws \Exception
+     */
+    public function resources()
+    {
+        $this->request(self::ACTIONS['resources']);
+        return $this->result();
+    }
+
+
+
+    /**
+     * @return mixed|string
+     */
+    public function result()
+    {
+        return $this->_result;
+    }
+
+    /**
+     * @param $link
+     * @param string $type
+     * @return bool
+     * @throws \Exception
+     */
+    private function request($link = NULL , $type = 'POST')
+    {
+        if($type === "GET")
+        {
+            try
+            {
+                if($type === 'GET')
+                {
+                    $this->get($this->_base_link.'/'.$this->_api_version.'/'.$link, $this->_params);
+                }
+                else if($type === 'POST')
+                {
+                    $this->post($this->_base_link.'/'.$this->_api_version.'/'.$link, $this->_params);
+                }
+                else throw new \Exception("Invalid Request");
+
+                $curl = json_decode($this->response);
+
+                if($curl->status === "OK")
+                {
+                    if(isset($curl->data)) $this->_result = $curl->data;
+                    return true;
+                }else
+                {
+                    $this->_result = $curl;
+                    return false;
+                }
+            }catch(\Exception $e)
+            {
+                return ($e->getMessage());
+            }
+        }
+        return false;
+
+    }
+
+}
